4420af62e62eed5ac35b2dadd9c2ec5728e697a0
OstadTahmasb <arman.tahmasebizadeh@gmail.com>
2023-12-06 20:38:36 +0330

    meow meow

--- calculator.c
+++ calculator.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int calculate(int a, int b){
-  // A different calculation
-  return a * b * 2 -10;
+	// A combination of both calculations
+	return a * b * 2 - 10;
+}
+
+int pow2(int a){
+	
+	return a*a;
 }
